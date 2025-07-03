f2abe7c469ab686b37945639614825fb0331f8c3
Igor Pavlov <sinigamimasters@gmail.com>
2025-07-03 19:41:04 +0300

    доделал FelineTest

--- src/test/java/FelineTest.java
+++ src/test/java/FelineTest.java
@@ -1,4 +1,31 @@
+import com.example.Feline;
+
+import org.junit.Test;
+import java.util.List;
 import static org.junit.Assert.assertEquals;
 
 public class FelineTest {
+
+    Feline feline = new Feline();
+
+    @Test
+    public void testEatMeat() throws Exception {
+        List<String> food = feline.eatMeat();
+        assertEquals(List.of("Животные", "Птицы", "Рыба"), food);
+    }
+
+    @Test
+    public void testGetFamily() {
+        assertEquals("Кошачьи", feline.getFamily());
+    }
+
+    @Test
+    public void testGetKittensDefault() {
+        assertEquals(1, feline.getKittens());
+    }
+
+    @Test
+    public void testGetKittensWithParam() {
+        assertEquals(3, feline.getKittens(3));
+    }
 }
