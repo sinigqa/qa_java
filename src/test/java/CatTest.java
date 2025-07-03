1b7d44e164220be850380ddb9c99366092cbe7f1
Igor Pavlov <sinigamimasters@gmail.com>
2025-07-03 20:42:25 +0300

    CatTest

---
+++ src/test/java/CatTest.java
@@ -0,0 +1,35 @@
+import com.example.Cat;
+import com.example.Predator;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
+
+public class CatTest {
+
+    private Predator mockPredator;
+    private Cat cat;
+
+    @Before
+    public void setUp() {
+        mockPredator = mock(Predator.class);
+        cat = new Cat(mockPredator);
+    }
+
+    @Test
+    public void testSound() {
+        assertEquals("Мяу", cat.getSound());
+    }
+
+    @Test
+    public void testGetFood() throws Exception {
+        when(mockPredator.eatMeat()).thenReturn(List.of("Мышки"));
+        List<String> food = cat.getFood();
+        verify(mockPredator).eatMeat();
+        assertEquals(List.of("Мышки"), food);
+    }
+}
